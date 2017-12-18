ed4295bd2a815a04f8d5a6f24d89f64de55fbaa5
beclawking <34658655+beclawking@users.noreply.github.com>
2017-12-18 18:51:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const Bot = new Discord.Client();
+
+Bot.login('MzkyMDMxNTQwNTcxNTM3NDA4.DRhTVw.5fr1Q-CmpjXGToY_Ki_VkR60jQU');
+
+//Listener Event: Bot Gestartet
+Bot.on('ready', () => {
+    console.log('Bot Gestartet...') //Konsolen Meldung Bot Gestartet...
+
+    //Status Online [Online,idle,invisible, & dnd]
+    //Bot.user.setStatus('Online')
+
+    //Spiel & Stream
+    //Bot.user.setGame('Hallo')
+
+});
+
+//Listener Event: User Joining The Discord Server.
+Bot.on('guildMemberAdd', member => {
+    
+    console.log('User ' + member.user.username + 'has joined the server!') //Send a message to the console that someone joined the discord server.
+
+    //Now lets add role when they join. First, we need to get the role we want.
+    var role = member.guild.roles.find('name', 'Team Sirion'); // This looks for the role in the server(guild), it searches by name, meaning you can change 'user' to the role you want!
+    
+    //Secondly, we will add the role
+    member.addRole(role)
+
+});
