bd2590f71498478c34b09dd7ca6e5282435157bc
beclawking <34658655+beclawking@users.noreply.github.com>
2018-09-03 22:36:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ Bot.on('guildMemberAdd', member => {
     console.log('User ' + member.user.username + 'has joined the server!') //Send a message to the console that someone joined the discord server.
 
     //Now lets add role when they join. First, we need to get the role we want.
-    var role = member.guild.roles.find('name', 'Team Sirion'); // This looks for the role in the server(guild), it searches by name, meaning you can change 'user' to the role you want!
+    var role = member.guild.roles.find('name', 'Raccrew'); // This looks for the role in the server(guild), it searches by name, meaning you can change 'user' to the role you want!
     
     //Secondly, we will add the role
     member.addRole(role)
